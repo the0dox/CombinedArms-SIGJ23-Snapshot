2e9ca0332a8f2df6b67fa128a1637d43f3202da5
donovan-westwater <donovan.westwater@gmail.com>
2023-11-05 18:22:56 -0800

    Hitscan sort of works. It doesnt work well when it misses. That should be fixed. Also sound should be implemented. Also add the object loader!

--- Seattle Indie Slow Jam 2023/Assets/Scripts/GunsAndPlayerControl/Gun.cs
+++ Seattle Indie Slow Jam 2023/Assets/Scripts/GunsAndPlayerControl/Gun.cs
@@ -9,9 +9,11 @@ public class Gun : MonoBehaviour
     public float reloadSpeed = 1f;
     public float ammoTotal = 100f;
     public float ammoPerBullet = 1f;
+    public float range = 10f;
     public AnimationClip fireAni;
     public AnimationClip reloadAni;
     public GameObject bullet;
+    public TrailRenderer hitscanBullet;
     float ammoCount;
     Animator animator;
     MeshRenderer renderer;
@@ -45,6 +47,42 @@ public class Gun : MonoBehaviour
         }
     }
     void FireWeapon()
+    {
+        Vector3 dir = this.transform.GetChild(0).position - this.transform.position;
+        RaycastHit info;
+        bool hit = Physics.Raycast(this.transform.GetChild(0).position, dir,out info,range);
+        if (hit && info.collider.tag == "Enemy") Debug.Log("HIT! THIS WOULD DAMAGE AN ENEMY");
+        TrailRenderer t = Instantiate(hitscanBullet, this.transform.GetChild(0).position, Quaternion.identity);
+        StartCoroutine(HitscanTrail(t,info));
+        //PUT DAMAGE CODE BELOW
+        //info.collider.GetComponent<Enemy>().takeDamage();
+        
+        ammoCount -= ammoPerBullet;
+        if (ammoCount < 0) ammoCount = 0;
+        float rVal = ammoCount / ammoTotal;
+        rVal = 2f * rVal - 1f;
+        rVal = -rVal;
+        rVal = Mathf.Clamp(rVal, -1f, 1f);
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        renderer.GetPropertyBlock(block);
+        block.SetFloat("_AmmoLevel", rVal);
+        renderer.SetPropertyBlock(block);
+
+    }
+    IEnumerator HitscanTrail(TrailRenderer t,RaycastHit info)
+    {
+        float time = 0;
+        Vector3 start = t.transform.position;
+        while(time < 1)
+        {
+            t.transform.position = Vector3.Lerp(start, info.point, time);
+            time += Time.deltaTime/t.time;
+            yield return null;
+        }
+        t.transform.position = info.point;
+        Destroy(t.gameObject, t.time);
+    }
+    void FireWeaponBullet()
     {
         GameObject g = Instantiate<GameObject>(bullet);
         g.transform.position = this.transform.GetChild(0).position;
