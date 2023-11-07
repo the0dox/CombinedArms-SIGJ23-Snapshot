d62f2205232bc9b35c4bd00351453e619656c0fc
donovan-westwater <donovan.westwater@gmail.com>
2023-11-06 19:13:39 -0800

    Fixed hitscan bug

--- Seattle Indie Slow Jam 2023/Assets/Scripts/GunsAndPlayerControl/Gun.cs
+++ Seattle Indie Slow Jam 2023/Assets/Scripts/GunsAndPlayerControl/Gun.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    //ScriptibleObject here -->>>
     public float firerate = 0.5f;
     public float dmg = 1f;
     public float reloadSpeed = 1f;
@@ -52,6 +53,10 @@ public class Gun : MonoBehaviour
         RaycastHit info;
         bool hit = Physics.Raycast(this.transform.GetChild(0).position, dir,out info,range);
         if (hit && info.collider.tag == "Enemy") Debug.Log("HIT! THIS WOULD DAMAGE AN ENEMY");
+        if (!hit)
+        {
+            info.point = this.transform.GetChild(0).position +  dir.normalized * range;
+        }
         TrailRenderer t = Instantiate(hitscanBullet, this.transform.GetChild(0).position, Quaternion.identity);
         StartCoroutine(HitscanTrail(t,info));
         //PUT DAMAGE CODE BELOW
