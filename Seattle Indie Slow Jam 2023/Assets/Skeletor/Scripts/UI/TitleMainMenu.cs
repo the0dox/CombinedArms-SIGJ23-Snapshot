73b34efd2a427215aa72767526fd0db51790d939
Austin Gunsauley <aj73090@msn.com>
2023-11-07 17:22:44 -0800

    Title screen

---
+++ Seattle Indie Slow Jam 2023/Assets/Skeletor/Scripts/UI/TitleMainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
