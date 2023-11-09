db995ffdab64cfbd61a332abdb42713d78d9ee59
The0dox <alexander.y.theodore@gmail.com>
2023-11-08 21:22:12 -0800

    Music Player Audio Source Simplification
    Changed the music player behavior to only require clips to be added instead of new audio sources

--- Seattle Indie Slow Jam 2023/Assets/Skeletor/Scripts/MusicPlayer.cs
+++ Seattle Indie Slow Jam 2023/Assets/Skeletor/Scripts/MusicPlayer.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // created by skeletor
@@ -10,100 +9,140 @@ public class MusicPlayer : MonoBehaviour
     // one instance of a music player in any scene
     private static MusicPlayer s_instance;
     // editable list of all tracks this music player can play
-    [SerializeField] private MusicLayer[] musicLayers;
-    // reference to output of music, used to adjust volume settings
-    private AudioSource currentMusic;
+    [SerializeField] private AudioClip[] _musicLayers;
     // the time in seconds at which songs fade in
     [SerializeField, Range(0, 1)] private float _fadeInSpeed; 
     // the time in seconds at which songs fade out
     [SerializeField, Range(0, 1)] private float _fadeOutSpeed; 
+    // reference the to the pair of audio sources attached to this behavior
+    private AudioSource[] _audioChannels;
+    // reference to index of the active audio source, used swap between sources when cross fading tracks
+    private static int s_activeSource;
 
     // assign instance before first frame
     void Awake()
     {
-        s_instance = this;
+        if(s_instance == null)
+        {
+            s_instance = this;
+            s_activeSource = 0;
+            _audioChannels = GetComponents<AudioSource>();
+        }
+        else
+        {
+            Debug.LogWarning("Multiple Music Players detected in the scene! For future reference there should only be one", gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    // unassign reference on scene change
+    void OnDestroy()
+    {
+        s_instance = null;
+    }
+
+    // plays a music layer from the clips name directly instead of by index 
+    public static void PlayLayer(string clipName)
+    {
+        for(int i = 0; i < s_instance._musicLayers.Length; i++)
+        {
+            if(s_instance._musicLayers[i].name.Equals(clipName))
+            {
+                PlayLayer(i);
+                return;
+            }
+        }
+        Debug.LogError($"Could not find clip with name {clipName} ensure that the clip is assigned in the music player prefab");
     }
 
     // finds an music layer that matches key and begins playing it
     // fades out any exisitng tracks 
-    public static void PlayLayer(string key)
+    public static void PlayLayer(int layer)
     {
-        float songLocation = 0;
-        if(s_instance.currentMusic != null)
+        AudioClip clip = s_instance._musicLayers[layer];
+        AudioSource activeChannel = s_instance._audioChannels[s_activeSource];
+        if(activeChannel.clip == clip)
         {
-            songLocation = s_instance.currentMusic.time;
+            Debug.Log($"Music Player was asked to play a Layer that was already active! (Layer {layer})");
+            return;
         }
-        foreach(MusicLayer layer in s_instance.musicLayers)
-        {
-            if(layer.key.Equals(key))
-            {
-                s_instance.StartCoroutine(PlayMusic(layer.music, songLocation));
-            }
-            else
-            {
-                s_instance.StartCoroutine(StopMusic(layer.music));
-            }
-        }
+        AudioSource inactiveChannel = s_instance._audioChannels[1 - s_activeSource];
+        float time = activeChannel.time;
+        s_instance.StartCoroutine(FadeOutPlayer(activeChannel));
+        s_instance.StartCoroutine(FadeInPlayer(inactiveChannel, time, clip));
+        s_activeSource = s_activeSource == 0 ? 1 : 0;
+        Debug.Log($"new source {s_activeSource}");
     }
 
     // fades out all music layers
-    private static void Stop()
+    public static void Stop()
     {
         s_instance.StopAllCoroutines();
-        foreach(MusicLayer layer in s_instance.musicLayers)
-        {
-            s_instance.StartCoroutine(StopMusic(layer.music));
-        }
-        s_instance.currentMusic = null;
+        s_instance.StartCoroutine(FadeOutPlayer(s_instance._audioChannels[0]));
+        s_instance.StartCoroutine(FadeOutPlayer(s_instance._audioChannels[1]));
+    }
+
+    // abruptly ends all music layers
+    public static void StopInstant()
+    {
+        s_instance.StopAllCoroutines();
+        s_instance._audioChannels[0].Stop();
+        s_instance._audioChannels[0].clip = null;
+        s_instance._audioChannels[1].Stop();
+        s_instance._audioChannels[1].clip = null;
     }
 
     // plays a specific layer, matching a songLocation for seamless fading
-    private static IEnumerator PlayMusic(AudioSource music, float songLocation)
-    {
-        s_instance.currentMusic = music;
-        music.time = songLocation;
-        music.volume = 0;
-        music.Play();
-        while(music.volume < 1)
+    private static IEnumerator FadeInPlayer(AudioSource player, float time, AudioClip clip)
+    {  
+        player.clip = clip;
+        player.time = time;
+        player.volume = 0;
+        player.Play();
+        while(player.volume < 1)
         {
-            music.volume += 0.01f;
+            player.volume += 0.01f;
             yield return new WaitForSeconds(s_instance._fadeInSpeed /100);
         }
-        music.volume = 1;
+        player.volume = 1;
     }
 
     // stops a specific layer
-    private static IEnumerator StopMusic(AudioSource music)
+    private static IEnumerator FadeOutPlayer(AudioSource player)
     {
-        while(music.volume > 0)
+        while(player.volume > 0)
         {
-            music.volume -= 0.01f;
+            player.volume -= 0.01f;
             yield return new WaitForSeconds(s_instance._fadeOutSpeed/100);
         }
-        music.volume = 0;
-        music.Stop();
+        player.Stop();
+        player.clip = null;
     }
 
-    // used for testing purposes
+    /* used for testing purposes
     #if UNITY_EDITOR
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            PlayLayer("layer 1");
+            PlayLayer(0);
         }
         else if(Input.GetKeyDown(KeyCode.Alpha2))
         {
-            PlayLayer("layer 2");
+            PlayLayer(1);
         }
         else if(Input.GetKeyDown(KeyCode.Alpha3))
         {
-            PlayLayer("layer 3");
+            PlayLayer(2);
         }
         else if(Input.GetKeyDown(KeyCode.Alpha0))
         {
             Stop();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PlayLayer("I'm invalid!");
         }
     }
 
@@ -115,25 +154,21 @@ public class MusicPlayer : MonoBehaviour
         }
         else if(GUILayout.Button("1: Play layer 1"))
         {
-            PlayLayer("layer 1");
+            PlayLayer(0);
         }
         else if(GUILayout.Button("2: Play layer 2"))
         {
-            PlayLayer("layer 2");
+            PlayLayer(1);
         }
         else if(GUILayout.Button("3: Play layer 3"))
         {
-            PlayLayer("layer 3");
+            PlayLayer(2);
+        }
+        else if(GUILayout.Button("4: test invalid string invoke"))
+        {
+            PlayLayer("I'm invalid!");
         }
     }
     #endif
-
-}
-
-// editor friendly struct for assigning audio sources to string keys
-[System.Serializable]
-public struct MusicLayer
-{
-    public string key;
-    public AudioSource music;
+    */
 }
