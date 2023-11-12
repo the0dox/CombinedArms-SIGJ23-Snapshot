43247c21665a8c9db0f42077999e83ad10155861
The0dox <alexander.y.theodore@gmail.com>
2023-11-11 19:40:47 -0800

    0.9 Weapon based Attacks + New Weapons
    Weapon Dependent Attacks
    -Each weapon now has its own AttackState field that returns a unique enemy attack state
    -added state factory static class
    -state factory returns new states after being given an enum key
    
    Added Shotgun Behavior
    -enemy shotgun now fires pellets in a cone
    -added enemy pellet projectile type that travels faster and deals less damage
    
    New Weapon: Pistol
    -added pistol
    -pistol uses the basic attack behavior previously assigned to shotguns
    
    -note that there are no unique weapon drops. All weapon drops spawn a shotgun model, this is a known issue and will be addressed later
    
    General Changes
    -added a separate shotgunner enemy prefab with adjusted attack range values.
    -regular enemy prefab can spawn with all 3 weapons
    -fixed an issue where projectiles were no longer despawning themselves after their lifesspawn had been depleted
    -started the work on a particle for charging a shotgun
    -discovered a glitch were enemies could spawn with two weapons

---
+++ Seattle Indie Slow Jam 2023/Assets/Skeletor/Scripts/Enemies/EnemyBurstAttackState.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// created by skeletor
+// enemy will look at player and shoot a burst of small projectiles in a cone
+public class EnemyBurstAttackState: State<EnemyBehavior>
+{
+    // time before the enemy fires after entering the state
+    private const float ATTACKWINDUP = 2f;
+    // the time before the enemy resumes regular behavior after attacking
+    private const float ATTACKWINDDOWN = 0.25f;
+    // the width of the weapon spread in radians
+    private const float WEAPONSPREAD = 30;
+    // the number of projectiles fired in a single burst
+    private const float NUMBEROFPELLETS = 10;
+
+    private float _attackTimer;
+
+    // allows class that derive State to 
+    protected override void OnStateEnter()
+    {
+        if(_myContext.LookTarget == null)
+        {
+            _myContext.SetState(_myContext.Idle);
+            return;
+        }
+        _myContext.ToggleNavAgent(false);
+        _myContext.AttackCoolDown = true;
+        //_myContext.transform.LookAt(new Vector3(_myContext.LookTarget.position.x ,_myContext.transform.position.y, _myContext.LookTarget.position.z));
+        _myContext.AnimationComponent.SetTrigger("Attack");
+        _attackTimer = ATTACKWINDUP;
+    }
+
+    // called every frame
+    protected override void OnStateUpdate()
+    {
+        _attackTimer -= Time.deltaTime;
+        if(_attackTimer < 0)
+        {
+            FireSpread();
+        }
+    }
+
+    // called every physics update frame
+    protected override void OnStateFixedUpdate()
+    {
+        _myContext.RotateTowardsTarget();
+    }
+
+    // called when this state is ended
+    protected override void OnStateExit()
+    {
+        _myContext.StartCoroutine(CoolDownDelay());
+    }
+
+    IEnumerator CoolDownDelay()
+    {
+        yield return new WaitForSeconds(_myContext.AttackCoolDownDuration);
+        _myContext.AttackCoolDown = false;
+    }
+
+    void FireSpread()
+    {
+        Vector3 attackVector = (_myContext.LookTarget.transform.position - _myContext.VisionTransform.position).normalized;
+        for(int i = 0; i < NUMBEROFPELLETS; i++)
+        {
+            float ang = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+            float yaw = Random.Range(-WEAPONSPREAD, WEAPONSPREAD) * Mathf.Deg2Rad;
+            Vector3 spread = new Vector3(Mathf.Sin(yaw)* Mathf.Cos(ang)
+            , Mathf.Sin(yaw)* Mathf.Sin(ang), Mathf.Sin(yaw)* Mathf.Cos(yaw));
+            Attack(attackVector + spread);
+        }
+        _myContext.SetState(_myContext.Approach);
+    }
+
+    void Attack(Vector3 attackVector)
+    {
+        GameObject projectile = ObjectLoader.LoadObject("Enemy Pellet", true);
+        projectile.transform.position = _myContext.VisionTransform.position + (attackVector * 2);
+        projectile.transform.rotation = Quaternion.LookRotation(attackVector);    
+    }
+}
+
