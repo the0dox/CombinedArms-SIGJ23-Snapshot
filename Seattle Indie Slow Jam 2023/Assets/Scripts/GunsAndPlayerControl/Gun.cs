734c868de0ab2fd56b18e9e284a852b4adb1a06d
The0dox <alexander.y.theodore@gmail.com>
2023-11-15 10:47:38 -0800

    Pause Screen Fix
    -Player controller is no longer allowed to pan the camera when game is paused
    -guns can no longer be fired or reloaded when screen is paused

--- Seattle Indie Slow Jam 2023/Assets/Scripts/GunsAndPlayerControl/Gun.cs
+++ Seattle Indie Slow Jam 2023/Assets/Scripts/GunsAndPlayerControl/Gun.cs
@@ -235,37 +235,40 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(PlayerManager.instance.gunsReloading);
-        //Debug.Log("GUn count: " + PlayerManager.instance.gunCount);
-        if (Input.GetMouseButton(0) && !isReloading && PlayerManager.instance.gunsReloading < 1
-            && (isAuto || !hasFired))
+        if(!PauseMenu.GameIsPaused)
         {
-            //Keep it nested so that reactions to running out of ammo can be handled
-            if (ammoCount > 0)
+            //Debug.Log(PlayerManager.instance.gunsReloading);
+            //Debug.Log("GUn count: " + PlayerManager.instance.gunCount);
+            if (Input.GetMouseButton(0) && !isReloading && PlayerManager.instance.gunsReloading < 1
+                && (isAuto || !hasFired))
             {
-                hasFired = true;
-                animator.SetBool("IsFiring", true);
-                isFiring = true;
+                //Keep it nested so that reactions to running out of ammo can be handled
+                if (ammoCount > 0)
+                {
+                    hasFired = true;
+                    animator.SetBool("IsFiring", true);
+                    isFiring = true;
+                }
+                else
+                {
+                    if (!barrel.isPlaying) barrel.PlayOneShot(clickSound);
+                    animator.SetBool("IsFiring", false);
+                    EndFire();
+                }
+                //FireWeapon();
             }
             else
             {
-                if (!barrel.isPlaying) barrel.PlayOneShot(clickSound);
+                if (Input.GetMouseButtonUp(0)) hasFired = false;
                 animator.SetBool("IsFiring", false);
                 EndFire();
             }
-            //FireWeapon();
-        }
-        else
-        {
-            if (Input.GetMouseButtonUp(0)) hasFired = false;
-            animator.SetBool("IsFiring", false);
-            EndFire();
-        }
-        if(Input.GetKeyDown(KeyCode.R) && !isReloading && ammoCount != ammoTotal)
-        {
-            isReloading = true;
-            PlayerManager.instance.gunsReloading++;
-            animator.SetBool("IsReloading", true);
+            if(Input.GetKeyDown(KeyCode.R) && !isReloading && ammoCount != ammoTotal)
+            {
+                isReloading = true;
+                PlayerManager.instance.gunsReloading++;
+                animator.SetBool("IsReloading", true);
+            }
         }
     }
 }
