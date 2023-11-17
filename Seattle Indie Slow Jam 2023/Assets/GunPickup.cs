78456ca8b99bc3d1c8409b06976c46b2258fd8fa
Austin Gunsauley <aj73090@msn.com>
2023-11-17 11:48:07 -0800

    Title screen background, weapon pickup scale increase + rotation

--- Seattle Indie Slow Jam 2023/Assets/GunPickup.cs
+++ Seattle Indie Slow Jam 2023/Assets/GunPickup.cs
@@ -6,6 +6,11 @@ public class GunPickup : MonoBehaviour
 {
     public GunData gun;
 
+    private void Update()
+    {
+        transform.Rotate(0, -1f, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(gun != null) PlayerManager.instance.PickupGun(gun);
